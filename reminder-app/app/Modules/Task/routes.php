689c08a542e01bd590fb30efe1abe961862c8c28
linerol <sessi.tchecounnou@epitech.eu>
2023-12-29 17:28:49 +0100

    [feat] : Tasks Module added to manage tasks

--- reminder-app/app/Modules/Task/routes.php
+++ reminder-app/app/Modules/Task/routes.php
@@ -4,9 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Modules\Task\Controllers\TaskController;
 
+Route::group(['middleware' => 'auth:sanctum'], function() {
 
-Route::get('tasks', [TaskController::class, 'index']);
-Route::post('tasks', [TaskController::class, 'store']);
-Route::get('tasks/{task}', [TaskController::class, 'show']);
-Route::match(['patch', 'put'], 'tasks/{task}', [TaskController::class, 'update']);
-Route::delete('tasks/{task}', [TaskController::class, 'destroy']);
+    Route::get('tasks', [TaskController::class, 'index']);
+    Route::post('tasks', [TaskController::class, 'store']);
+    Route::get('tasks/{task}', [TaskController::class, 'show']);
+    Route::match(['patch', 'put'], 'tasks/{task}', [TaskController::class, 'update']);
+    Route::delete('tasks/{task}', [TaskController::class, 'destroy']);
+});
