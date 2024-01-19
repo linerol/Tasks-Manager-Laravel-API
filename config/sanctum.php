112934871312edbdb51283ec22b45f564d277978
linerol <sessi.tchecounnou@epitech.eu>
2024-01-19 14:18:36 +0100

    [fix] :  403 forbidden error in progress to be fixed

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,10 +17,14 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,https://reminder-app-nine-eta.vercel.app',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
+    // 'allowed_domains' => [
+    //     'https://reminder-app-nine-eta.vercel.app',
+    // ],
+    
     /*
     |--------------------------------------------------------------------------
     | Sanctum Guards
