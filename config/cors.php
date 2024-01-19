0cd473568a114912893bc86c82ad8b0c5a6eb9f3
linerol <sessi.tchecounnou@epitech.eu>
2024-01-19 15:13:43 +0100

    [fix] :  403 forbidden error in progress to be fixed

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://reminder-app-nine-eta.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
